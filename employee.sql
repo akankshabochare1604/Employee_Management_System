e4b91c55d98973266d5c309075d7c1504a0a6d2a
akankshabochare1604 <147162527+akankshabochare1604@users.noreply.github.com>
2025-02-20 18:49:11 +0530

    Add files via upload

---
+++ employee.sql
@@ -0,0 +1,18 @@
+create table employee
+(emp_id int primary key auto_increment,
+fname varchar(30)not null,
+lname varchar(30)not null,
+gender varchar(15)not null,
+email varchar(50)unique not null,
+mobno bigint unique not null,
+dob date not null,
+age int not null,
+salary bigint not null,
+role varchar(30)not null,
+join_date date not null,
+ experience int not null,
+city varchar(30)not null,
+ state varchar(30)not null,
+country varchar(30)not null,
+marital_status varchar(20)not null
+,password varchar(8));
